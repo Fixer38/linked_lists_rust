6b2a954396e51c12379d8aeb0708fd3768a9c237
TheWolfInstinct <fixer3838@live.fr>
2021-04-19 23:54:17 +0200

    ADDED peek and peek_mut methods to get the first element of the linked list + ADDED test for specified methods

--- src/second.rs
+++ src/second.rs
@@ -28,9 +28,22 @@ impl<T> List<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
-            })
-        }
+        })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
+}
+
 
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
@@ -65,5 +78,21 @@ mod test {
 
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| {
+            *value = 64;
+        });
+
+        assert_eq!(list.peek(), Some(&64));
+        assert_eq!(list.pop(), Some(64));
     }
 }
