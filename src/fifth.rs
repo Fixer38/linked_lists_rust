4457c6ece0be8a44cb64aa6088cbdedac0ef8745
TheWolfInstinct <fixer3838@live.fr>
2021-04-27 23:33:46 +0200

    ADDED pop method

--- src/fifth.rs
+++ src/fifth.rs
@@ -41,4 +41,17 @@ impl<'a, T> List<'a ,T> {
         };
         self.tail = new_tail;
     }
+
+    pub fn pop(&'a mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
+
+            // Check if there is no more head
+            if self.head.is_none() {
+                self.tail = None;
+            }
+            head.elem
+        })
+    }
 }
