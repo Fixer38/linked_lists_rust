26dae440ba72e8ddcc510223ba34046b24f12324
TheWolfInstinct <fixer3838@live.fr>
2021-04-27 23:31:36 +0200

    ADDED fifth list (unsafe deque) + ADDED push method

---
+++ src/fifth.rs
@@ -0,0 +1,44 @@
+use std::mem;
+
+pub struct List<'a, T> {
+    head: Link<T>,
+    tail: Option<&'a mut Node<T>>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<'a, T> List<'a ,T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None
+        }
+    }
+
+    pub fn push(&'a mut self, elem: T) {
+        let new_tail = Box::new(Node {
+            elem: elem,
+            next: None,
+        });
+
+        // Put Box at the right place and grab reference to its node
+        let new_tail = match self.tail.take() {
+            // Non empty list
+            Some(old_tail) => {
+                old_tail.next = Some(new_tail);
+                old_tail.next.as_deref_mut()
+            }
+            // Empty list
+            None => {
+                self.head = Some(new_tail);
+                self.head.as_deref_mut()
+            }
+        };
+        self.tail = new_tail;
+    }
+}
