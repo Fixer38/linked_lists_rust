ece661bf16b456991f4246174a93e7ea950ab6f3
TheWolfInstinct <fixer3838@live.fr>
2021-04-27 23:36:56 +0200

    ADDED tests (currently failing, need to fix it)

--- src/fifth.rs
+++ src/fifth.rs
@@ -55,3 +55,23 @@ impl<'a, T> List<'a ,T> {
         })
     }
 }
+
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), None);
+
+    }
+}
