44b3fa168ebff4993aa6ee40eb73a2f7a4ecc15b
TheWolfInstinct <fixer3838@live.fr>
2021-04-29 23:33:09 +0200

    REMOVED pop method + MODIFIED tail to raw pointer due to reference to self + MODIFIED push_method and new method to use said raw pointer

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,8 +1,8 @@
 use std::mem;
 
-pub struct List<'a, T> {
+pub struct List<T> {
     head: Link<T>,
-    tail: Option<&'a mut Node<T>>,
+    tail:  *mut Node<T>,
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -12,37 +12,32 @@ struct Node<T> {
     next: Link<T>,
 }
 
-impl<'a, T> List<'a ,T> {
+impl<'a, T> List<T> {
     pub fn new() -> Self {
         List {
             head: None,
-            tail: None
+            tail: std::ptr::null_mut()
         }
     }
 
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node {
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node {
             elem: elem,
             next: None,
         });
 
-        // Put Box at the right place and grab reference to its node
-        let new_tail = match self.tail.take() {
-            // Non empty list
-            Some(old_tail) => {
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
-            }
-            // Empty list
-            None => {
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
-        self.tail = new_tail;
+        let raw_tail: *mut _ = &mut *new_tail;
+
+        if !self.tail.is_null() {
+            unsafe { (*self.tail).next = Some(new_tail); }
+        }
+        else {
+            self.head = Some(new_tail);
+        }
+        self.tail = raw_tail;
     }
 
-    pub fn pop(&'a mut self) -> Option<T> {
+    /*pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|head| {
             let head = *head;
             self.head = head.next;
@@ -53,7 +48,7 @@ impl<'a, T> List<'a ,T> {
             }
             head.elem
         })
-    }
+    }*/
 }
 
 mod test {
